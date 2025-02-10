489c21083ed395772379cc1bfcc37f1733f47516
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2025-02-10 15:49:13 +0530

    Resume editor layout

---
+++ src/app/(main)/editor/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+import ResumeEditor from "./ResumeEditor";
+
+export const metadata: Metadata = {
+  title: "Design your resume",
+};
+export default function Page() {
+  return <ResumeEditor />;
+}
